7dca378097f8aacbf89e5ae8fb0546081615c31c
fairflow <matthew@fairtlough.net>
2025-05-16 11:16:53 +0100

    Extended LaxND and added simple proofs

--- LaxLogic/PLLNDProof.lean
+++ LaxLogic/PLLNDProof.lean
@@ -8,13 +8,46 @@ inductive LaxND : (List PLLFormula)→ PLLFormula → Prop -- Natural deduction 
 -- this first is not sufficiently general
   | iden : (Γ Δ : List PLLFormula) → (φ : PLLFormula) → LaxND (Γ ++ [φ] ++ Δ) φ
   | falsoElim : {Γ : List PLLFormula} → (φ : PLLFormula) → LaxND Γ falsePLL → LaxND Γ φ
-  | impIntro : {Γ Δ : List PLLFormula} → {φ ψ : PLLFormula} → LaxND (Γ ++ [φ] ++ Δ) ψ → LaxND (Γ ++ Δ) (ifThen φ ψ)
+  | impIntro : {Γ Δ : List PLLFormula} → {φ ψ : PLLFormula} →
+      LaxND (Γ ++ [φ] ++ Δ) ψ → LaxND (Γ ++ Δ) (ifThen φ ψ)
   | impElim : {Γ : List PLLFormula} → {φ ψ : PLLFormula} → LaxND Γ (ifThen φ ψ) → LaxND Γ φ → LaxND Γ ψ
+  | andIntro : {Γ : List PLLFormula} → {φ ψ : PLLFormula} → LaxND Γ φ → LaxND Γ ψ → LaxND Γ (and φ ψ)
+  | andElim1 : {Γ : List PLLFormula} → {φ ψ : PLLFormula} → LaxND Γ (and φ ψ) → LaxND Γ φ
+  | andElim2 : {Γ : List PLLFormula} → {φ ψ : PLLFormula} → LaxND Γ (and φ ψ) → LaxND Γ ψ
   | laxIntro : {Γ : List PLLFormula} → {φ : PLLFormula} → LaxND Γ φ → LaxND Γ (somehow φ)
-  | laxElim : {Γ Δ : List PLLFormula} → {φ ψ : PLLFormula} → LaxND (Γ ++ Δ) (somehow φ)→ LaxND (Γ ++ [φ] ++ Δ) (somehow ψ) → LaxND (Γ ++ Δ) (somehow ψ)
+  | laxElim : {Γ Δ : List PLLFormula} → {φ ψ : PLLFormula} →
+      LaxND (Γ ++ Δ) (somehow φ)→ LaxND (Γ ++ [φ] ++ Δ) (somehow ψ) → LaxND (Γ ++ Δ) (somehow ψ)
 --  | laxFlatten : {Γ : List PLLFormula} → {φ : PLLFormula} → LaxND Γ (somehow (somehow φ)) → LaxND Γ (somehow φ)
 -- this last is derivable
 
-infix:70 "⊢⋆" => LaxND -- turnstile
+infix:70 " ⊢- " => LaxND -- turnstile
 
-def LaxValid (φ : PLLFormula) : Prop := ([] : List PLLFormula) ⊢⋆ φ
+def LaxValid (φ : PLLFormula) : Prop := ([] : List PLLFormula) ⊢- φ
+
+open LaxND
+
+lemma OI (φ : PLLFormula) : [] ⊢- ifThen φ (somehow φ) := impIntro (laxIntro (iden [] [] φ))
+lemma OItrue (φ : PLLFormula) : LaxValid <| ifThen φ (somehow φ) := by apply OI
+
+lemma OSL (φ ψ : PLLFormula) : [] ⊢- ifThen (somehow (and φ ψ)) (and (somehow φ) (somehow ψ)) := by
+  apply @impIntro [] [] ;
+  apply @andIntro;
+  apply @laxElim [(φ.and ψ).somehow] [] (φ.and ψ);
+  apply iden [] ; apply laxIntro
+  apply andElim1 ; apply iden
+  apply @laxElim [(φ.and ψ).somehow] [] (φ.and ψ);
+  apply iden [] ; apply laxIntro
+  apply andElim2 ; apply iden
+lemma OSLtrue (φ ψ : PLLFormula) : LaxValid <| ifThen (somehow (and φ ψ)) (and (somehow φ) (somehow ψ)) := by apply OSL
+
+lemma OMoops {Γ : List PLLFormula} {φ : PLLFormula} : (Γ ++ []) ⊢- ifThen (somehow (somehow φ)) (somehow φ) :=
+  impIntro (
+    laxElim
+      (iden Γ [] (somehow (somehow φ)))
+      (iden (Γ ++ [φ.somehow.somehow]) [] (somehow φ))
+  )
+-- well I did think that way of permuting contexts might throw up problems!
+lemma OM {φ : PLLFormula} : [] ⊢- ifThen (somehow (somehow φ)) (somehow φ) := by
+  apply @impIntro [] ; simp;  apply @laxElim [(somehow (somehow φ))] [] (somehow φ) ; simp;
+  apply iden [] ; apply iden
+lemma OMtrue (φ : PLLFormula) : LaxValid <| ifThen (somehow (somehow φ)) (somehow φ) := by apply OM
